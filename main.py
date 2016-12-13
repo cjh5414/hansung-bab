ba5a281f4fcf84c63d616675b57d605535dfa0a7
cjh5414 <cjh5414@gmail.com>
2016-12-13 18:27:26 +0900

    Get menu table from response

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import urllib3
+from bs4 import BeautifulSoup
 
+urllib3.disable_warnings()
 
+URL = 'https://www.hansung.ac.kr/web/www/life_03_01_t2'
+
+http = urllib3.PoolManager()
+response = http.request('GET', URL)
+soup = BeautifulSoup(response.data, 'html.parser')
+table = soup.findAll('table')[0]
+print(table)
