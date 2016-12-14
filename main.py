70e392963295f0610a27ae13862369e4657d9a68
cjh5414 <cjh5414@gmail.com>
2016-12-14 17:05:12 +0900

    Refactor getting menu

--- main.py
+++ main.py
@@ -5,25 +5,18 @@ urllib3.disable_warnings()
 
 URL = 'https://www.hansung.ac.kr/web/www/life_03_01_t2'
 
-http = urllib3.PoolManager()
-response = http.request('GET', URL)
-soup = BeautifulSoup(response.data, 'html.parser')
-table = soup.findAll('table')[0]
-td_list = table.findAll('td')
-
 lunch_menu_list = []
 dinner_menu_list = []
 
-for i in range(5):
-    lunch_menu_list.append(td_list[i].get_text(' ').split())
-    dinner_menu_list.append(td_list[i+7].get_text(' ').split())
+def get_menu_divided_by_days_of_the_week():
+    http = urllib3.PoolManager()
+    response = http.request('GET', URL)
+    soup = BeautifulSoup(response.data, 'html.parser')
+    table = soup.findAll('table')[0]
+    td_list = table.findAll('td')
 
-for i in range(5):
-    print(str(i))
-    for menu in lunch_menu_list[i]:
-        print(menu)
+    for i in range(5):
+        lunch_menu_list.append(td_list[i].get_text(' ').split())
+        dinner_menu_list.append(td_list[i+7].get_text(' ').split())
 
-    print()
 
-    for menu in  dinner_menu_list[i]:
-        print(menu)
