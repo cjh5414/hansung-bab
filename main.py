7c36da5d2ae18c02957a1b8b0c6a3c8645bb94a6
cjh5414 <cjh5414@gmail.com>
2016-12-14 17:33:43 +0900

    Get todya's menu

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 import urllib3
 from bs4 import BeautifulSoup
+
 
 urllib3.disable_warnings()
 
@@ -7,6 +10,7 @@ URL = 'https://www.hansung.ac.kr/web/www/life_03_01_t2'
 
 lunch_menu_list = []
 dinner_menu_list = []
+
 
 def get_menu_divided_by_days_of_the_week():
     http = urllib3.PoolManager()
@@ -19,4 +23,11 @@ def get_menu_divided_by_days_of_the_week():
         lunch_menu_list.append(td_list[i].get_text(' ').split())
         dinner_menu_list.append(td_list[i+7].get_text(' ').split())
 
+
+def get_todays_menu():
+    today_index = datetime.today().weekday()
+    for menu in lunch_menu[today_index]:
+        print(menu)
+    for menu in dinner_menu[today_index]:
+        print(menu)
 
